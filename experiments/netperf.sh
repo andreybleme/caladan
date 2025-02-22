f21edc111284d23459b48a95a360eea61465e41a
andreybleme <andreybleme1@gmail.com>
2025-02-22 14:03:43 -0300

    chore: dpdk_netperf experiment results and updates to make app compile

--- experiments/netperf.sh
+++ experiments/netperf.sh
@@ -2,9 +2,28 @@
 sudo apt install netperf
 
 # latency (req/res perf)
-netperf -H 128.110.218.90 -t TCP_RR -l 5
+netperf -H 128.110.218.59 -p 5000 -t TCP_RR -l 5
 
 # throughput 
-etperf -H 128.110.218.90 -t TCP_STREAM -l 5
+etperf -H 128.110.218.59 -p 5000 -t TCP_STREAM -l 5
 
 # https://blog.jtouzi.net/measuring-network-throughput-netperf-iperf/
+
+
+### Run netperf DPDK app ===========================
+export RTE_SDK=/users/lbleme/caladan/dpdk
+
+# copy from caladan/apps to dpdk submodule folder (use helloword Makefile as reference)
+cd ~/caladan/dpdk/examples/dpdk_netperf
+cp -r ../../../apps/dpdk_netperf/ .
+# create meson.build file
+
+meson configure -Dexamples=dpdk_netperf
+ninja
+
+# run the built version
+cd ~/caladan/dpdk/build
+# server
+sudo ./examples/dpdk-dpdk_netperf -l2 --socket-mem=128 -- UDP_SERVER 128.110.218.59
+# client
+sudo ./examples/dpdk-dpdk_netperf -l2 --socket-mem=128 -- UDP_CLIENT 128.110.218.44 128.110.218.59 50000 8001 10 8 50
